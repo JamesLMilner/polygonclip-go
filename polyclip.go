b698c360213a893d1fec1a148c46ed31c7770604
James Milner <jamesmilner22@hotmail.com>
2016-06-08 16:22:24 +0100

    More fixing

--- polyclip.go
+++ polyclip.go
@@ -1,360 +1,363 @@
-    package main
+package main
 
-    type Point struct {
-	    X float64
-	    Y float64
-    }
+import "reflect"
 
-    type Polygon struct {
-	    Points []Point
-    }
+type Point struct {
+	X float64
+	Y float64
+}
 
-    type Node struct {
-	    Poly      Polygon
-	    Index     int
-	    Point     Point
-	    Intersect bool
-	    Next      *Node
-	    Prev      *Node
-	    IsEntry   bool
-	    Friend    *Node
-	    Processed bool
-    }
+type Polygon struct {
+	Points []Point
+}
 
-    type Intersection struct {
-	    Cross  float64
-	    AlongA float64
-	    AlongB float64
-	    Point  Point
-    }
+type Node struct {
+	Poly      Polygon
+	Index     int
+	Point     Point
+	Intersect bool
+	Dist      float64
+	Next      *Node
+	Prev      *Node
+	IsEntry   bool
+	Friend    *Node
+	Processed bool
+}
 
-    type Result struct {
-	    Result  Polygon
-	    RootOne Node
-	    RootTwo Node
-    }
+type Intersection struct {
+	Cross  float64
+	AlongA float64
+	AlongB float64
+	Point  Point
+}
 
-    func UpgradePolygon(polygon Polygon) Node {
+type Result struct {
+	Result  Polygon
+	RootOne Node
+	RootTwo Node
+}
 
-	    // converts a list of points into a double linked list
-	    var root *Node
-	    var prev *Node
+func UpgradePolygon(polygon Polygon) Node {
 
-	    for i := 0; i < len(polygon.Points); i++ {
+	// converts a list of points into a double linked list
+	var root *Node
+	var prev *Node
 
-		    node := &Node{
+	for i := 0; i < len(polygon.Points); i++ {
 
-			    Poly:         polygon, // extra data to help the UI -- unnecessary for algorithm to work:
-			    Index:        i,       // extra data to help the UI -- unnecessary for algorithm to work:
-			    Point:        polygon.Points[i],
-			    Intersect:    false,
-			    Next:         nil,
-			    Prev:         nil,
-		    }
+		node := &Node{
 
-		    if root == nil { // root just points to itself:
+			Poly:      polygon, // extra data to help the UI -- unnecessary for algorithm to work:
+			Index:     i,       // extra data to help the UI -- unnecessary for algorithm to work:
+			Point:     polygon.Points[i],
+			Intersect: false,
+			Next:      nil,
+			Prev:      nil,
+		}
 
-			    node.Next = node
-			    node.Prev = node
-			    root = node
+		if root == nil { // root just points to itself:
 
-		    } else {
+			node.Next = node
+			node.Prev = node
+			root = node
 
-			    prev = root.Prev // change this:
-			    prev.Next = node //    ...-- (prev) <--------------> (root) --...
-			    node.Prev = prev // to this:
-			    node.Next = root //    ...-- (prev) <--> (node) <--> (root) --...
-			    root.Prev = node
+		} else {
 
-		    }
+			prev = root.Prev // change this:
+			prev.Next = node //    ...-- (prev) <--------------> (root) --...
+			node.Prev = prev // to this:
+			node.Next = root //    ...-- (prev) <--> (node) <--> (root) --...
+			root.Prev = node
 
-	    }
+		}
 
-	    return *root
+	}
 
-    }
+	return *root
 
-    func LinesIntersect(aZero Point, aOne Point, bZero Point, bOne Point) Intersection {
+}
 
-	    adX := aOne.X - aZero.X
-	    adY := aOne.Y - aZero.Y
-	    bdX := bOne.X - bZero.X
-	    bdY := bOne.Y - bZero.Y
+func LinesIntersect(aZero Point, aOne Point, bZero Point, bOne Point) Intersection {
 
-	    axb := adX*bdY - adY*bdX
+	adX := aOne.X - aZero.X
+	adY := aOne.Y - aZero.Y
+	bdX := bOne.X - bZero.X
+	bdY := bOne.Y - bZero.Y
 
-	    intersect := Intersection{}
+	axb := adX*bdY - adY*bdX
 
-	    if axb == 0 {
+	intersect := Intersection{}
 
-		    return intersect
+	if axb == 0 {
 
-	    }
+		return intersect
 
-	    dx := aZero.X - bZero.X
-	    dy := aZero.Y - bZero.Y
+	}
 
-	    intersect.AlongA = (bdX*dy - bdY*dx) / axb
-	    intersect.AlongB = (adX*dy - adY*dx) / axb
+	dx := aZero.X - bZero.X
+	dy := aZero.Y - bZero.Y
 
-	    intersect.Point = Point{
-		    X : aZero.X + intersect.AlongA*adX,
-		    Y : aZero.Y + intersect.AlongA*adY,
-	    }
+	intersect.AlongA = (bdX*dy - bdY*dx) / axb
+	intersect.AlongB = (adX*dy - adY*dx) / axb
 
-	    return intersect
+	intersect.Point = Point{
+		X: aZero.X + intersect.AlongA*adX,
+		Y: aZero.Y + intersect.AlongA*adY,
+	}
 
-    }
+	return intersect
 
-    func NextNonIntersection(node *Node) *Node {
+}
 
-	    for node.Intersect {
+func NextNonIntersection(node *Node) *Node {
 
-		    node = node.Next
+	for node.Intersect {
 
-	    }
+		node = node.Next
 
-	    return node
+	}
 
-    }
+	return node
 
-    func PointInPolygon(point Point, root Node) bool {
+}
 
-	    isOdd := false
-	    x := point.X
-	    y := point.Y
-	    here := root
+func PointInPolygon(point Point, root *Node) bool {
 
-	    var (
-		    next Node
-		    hx   float64
-		    hy   float64
-		    nx   float64
-		    ny   float64
-		    pip  bool
-	    )
+	isOdd := false
+	x := point.X
+	y := point.Y
+	here := root
 
-	    for here != root {
+	var (
+		next *Node
+		hx   float64
+		hy   float64
+		nx   float64
+		ny   float64
+		pip  bool
+	)
 
-		    next = here.Next
-		    hx = here.Point.X
-		    hy = here.Point.Y
-		    nx = next.Point.X
-		    ny = next.Point.Y
+	for reflect.DeepEqual(here, root) == false {
 
-		    pip = (((hy < y && ny >= y) || (hy >= y && ny < y)) &&
-			    (hx <= x || nx <= x) &&
-			    (hx+(y-hy)/(ny-hy)*(nx-hx) < x))
+		next = here.Next
+		hx = here.Point.X
+		hy = here.Point.Y
+		nx = next.Point.X
+		ny = next.Point.Y
 
-		    if pip {
+		pip = (((hy < y && ny >= y) || (hy >= y && ny < y)) &&
+			(hx <= x || nx <= x) &&
+			(hx+(y-hy)/(ny-hy)*(nx-hx) < x))
 
-			    isOdd = !isOdd
+		if pip {
 
-		    }
+			isOdd = !isOdd
 
-		    here = next
+		}
 
-	    }
+		here = next
 
-	    return isOdd
+	}
 
-    }
+	return isOdd
 
-    func CalculateEntryExit(root Node, isEntry bool) {
+}
 
-	    here := root
+func CalculateEntryExit(root *Node, isEntry bool) {
 
-	    for here != root {
+	here := root
 
-		    if here.Intersection {
+	for reflect.DeepEqual(here, root) == false {
 
-			    here.isEntry = isEntry
-			    isEntry = !isEntry
+		if here.Intersect {
 
-		    }
+			here.IsEntry = isEntry
+			isEntry = !isEntry
 
-		    here = here.next
+		}
 
-	    }
+		here = here.Next
 
-    }
+	}
 
-    func PolygonClip(polyOne Polygon, polyTwo Polygon, intoRed bool, intoBlue bool) {
+}
 
-	    rootOne := UpgradePolygon(polyOne)
-	    rootTwo := UpgradePolygon(polyTwo)
+func PolygonClip(polyOne Polygon, polyTwo Polygon, intoRed bool, intoBlue bool) {
 
-	    // do this before inserting intersections, for simplicity
-	    isOneInTwo := PointInPolygon(rootOne.point, rootTwo)
-	    isTwoInOne := PointInPolygon(rootTwo.point, rootOne)
+	rootOne := UpgradePolygon(polyOne)
+	rootTwo := UpgradePolygon(polyTwo)
 
-	    hereOne := rootOne
-	    hereTwo := rootTwo
+	// do this before inserting intersections, for simplicity
+	isOneInTwo := PointInPolygon(rootOne.Point, &rootTwo)
+	isTwoInOne := PointInPolygon(rootTwo.Point, &rootOne)
 
-	    for hereOne == rootOne {
+	hereOne := rootOne
+	hereTwo := rootTwo
 
-		    for hereTwo == rootTwo {
+	for reflect.DeepEqual(hereOne, rootOne) {
 
-			    var next1 = NextNonIntersection(hereOne)
-			    var next2 = NextNonIntersection(hereTwo)
+		for reflect.DeepEqual(hereTwo, rootTwo) {
 
-			    var intersect = LinesIntersect(
-				    hereOne.Point, next1.Point,
-				    hereTwo.Point, next2.Point,
-			    )
+			var next1 = NextNonIntersection(&hereOne)
+			var next2 = NextNonIntersection(&hereTwo)
 
-			    if intersect.alongA > 0 && intersect.alongA < 1 &&
-				    intersect.alongB > 0 && intersect.alongB < 1 {
+			var intersect = LinesIntersect(
+				hereOne.Point, next1.Point,
+				hereTwo.Point, next2.Point,
+			)
 
-				    nodeOne := Node{
-					    Point:        intersect.Point,
-					    Intersection: true,
-					    Next:         nil,
-					    Prev:         nil,
-					    Dist:         intersect.AlongA,
-					    Friend:       nil,
-				    }
+			if intersect.AlongA > 0 && intersect.AlongA < 1 &&
+				intersect.AlongB > 0 && intersect.AlongB < 1 {
 
-				    nodeTwo := Node{
-					    Point:        intersect.Point,
-					    Intersection: true,
-					    Next:         nil,
-					    Prev:         nil,
-					    Dist:         intersect.AlongB,
-					    Friend:       nil,
-				    }
+				nodeOne := &Node{
+					Point:     intersect.Point,
+					Intersect: true,
+					Next:      nil,
+					Prev:      nil,
+					Dist:      intersect.AlongA,
+					Friend:    nil,
+				}
 
-				    // point the nodes at each other
-				    nodeOne.Friend = nodeTwo
-				    nodeTwo.Friend = nodeOne
+				nodeTwo := &Node{
+					Point:     intersect.Point,
+					Intersect: true,
+					Next:      nil,
+					Prev:      nil,
+					Dist:      intersect.AlongB,
+					Friend:    nil,
+				}
 
-				    var inext Node
-				    var iprev Node
+				// point the nodes at each other
+				nodeOne.Friend = nodeTwo
+				nodeTwo.Friend = nodeOne
 
-				    // find insertion between hereOne and next1, based on dist
-				    inext = hereOne.Next
-				    for inext != next1 && inext.Dist < nodeOne.Dist {
-					    inext = inext.Next
-				    }
+				var inext *Node
+				var iprev *Node
 
-				    iprev = inext.Prev
+				// find insertion between hereOne and next1, based on dist
+				inext = hereOne.Next
+				for inext != next1 && inext.Dist < nodeOne.Dist {
+					inext = inext.Next
+				}
 
-				    // insert nodeOne between iprev and inext
-				    inext.Prev = nodeOne
-				    nodeOne.Next = inext
-				    nodeOne.Prev = iprev
-				    iprev.Next = nodeOne
+				iprev = inext.Prev
 
-				    // find insertion between hereTwo and next2, based on dist
-				    inext = hereTwo.Next
+				// insert nodeOne between iprev and inext
+				inext.Prev = nodeOne
+				nodeOne.Next = inext
+				nodeOne.Prev = iprev
+				iprev.Next = nodeOne
 
-				    for inext != next2 && inext.Dist < nodeTwo.Dist {
-					    inext = inext.Next
-				    }
+				// find insertion between hereTwo and next2, based on dist
+				inext = hereTwo.Next
 
-				    iprev = inext.Prev
+				for inext != next2 && inext.Dist < nodeTwo.Dist {
+					inext = inext.Next
+				}
 
-				    // insert nodeTwo between iprev and inext
-				    inext.Prev = nodeTwo
-				    nodeTwo.Next = inext
-				    nodeTwo.Prev = iprev
-				    iprev.Next = nodeTwo
+				iprev = inext.Prev
 
-			    }
+				// insert nodeTwo between iprev and inext
+				inext.Prev = nodeTwo
+				nodeTwo.Next = inext
+				nodeTwo.Prev = iprev
+				iprev.Next = nodeTwo
 
-			    hereTwo = NextNonIntersection(hereTwo)
+			}
 
-		    }
+			hereTwo = NextNonIntersection(&hereTwo)
 
-		    hereOne = NextNonIntersection(hereOne)
+		}
 
-	    }
+		hereOne = NextNonIntersection(&hereOne)
 
-	    CalculateEntryExit(rootOne, !isOneInTwo)
-	    CalculateEntryExit(rootTwo, !isTwoInOne)
+	}
 
-	    var result Polygon
-	    sect := rootOne
-	    into := [2]bool{intoBlue, intoRed}
+	CalculateEntryExit(rootOne, !isOneInTwo)
+	CalculateEntryExit(rootTwo, !isTwoInOne)
 
-	    for true {
+	var result Polygon
+	sect := rootOne
+	into := [2]bool{intoBlue, intoRed}
 
-		    for isect != rootOne {
+	for true {
 
-			    if isect.Intersection && !isect.Processed {
-				    break
-			    }
+		for isect != rootOne {
 
-			    isect = isect.Next
+			if isect.Intersection && !isect.Processed {
+				break
+			}
 
-		    }
+			isect = isect.Next
 
-		    if isect == rootOne {
+		}
 
-			    break
+		if isect == rootOne {
 
-		    }
+			break
 
-		    curpoly := 0
-		    clipped := []float64{}
+		}
 
-		    here := isect
+		curpoly := 0
+		clipped := []float64{}
 
-		    for !here.Processed {
+		here := isect
 
-			    // mark intersection as processed
-			    here.Processed = true
-			    here.Friend.Processed = true
+		for !here.Processed {
 
-			    var moveForward = here.IsEntry == into[curpoly]
+			// mark intersection as processed
+			here.Processed = true
+			here.Friend.Processed = true
 
-			    for !here.Intersection {
+			var moveForward = here.IsEntry == into[curpoly]
 
-				    append(clipped, here.Point)
+			for !here.Intersection {
 
-				    if moveForward {
+				append(clipped, here.Point)
 
-					    here = here.Next
+				if moveForward {
 
-				    } else {
+					here = here.Next
 
-					    here = here.Prev
+				} else {
 
-				    }
+					here = here.Prev
 
-				    // we've hit the next intersection so switch polygons
-				    here = here.Friend
-				    curpoly = 1 - curpoly
+				}
 
-			    }
+				// we've hit the next intersection so switch polygons
+				here = here.Friend
+				curpoly = 1 - curpoly
 
-			    append(result, clipped)
+			}
 
-		    }
+			append(result, clipped)
 
-		    if len(result) <= 0 {
+		}
 
-			    if isOneInTwo == intoBlue {
+		if len(result) <= 0 {
 
-				    append(result, polyOne)
+			if isOneInTwo == intoBlue {
 
-			    }
+				append(result, polyOne)
 
-			    if isTwoInOne == intoRed {
+			}
 
-				    append(result, polyTwo)
+			if isTwoInOne == intoRed {
 
-			    }
+				append(result, polyTwo)
 
-		    }
+			}
 
-		    return Result{
-			    RootOne: rootOne, // used for UI
-			    RootTwo: rootTwo, // used for UI
-			    Result:  result,  // this is all you really need
-		    }
+		}
 
-	    }
+		return Result{
+			RootOne: rootOne, // used for UI
+			RootTwo: rootTwo, // used for UI
+			Result:  result,  // this is all you really need
+		}
 
-    }
+	}
+
+}
 
